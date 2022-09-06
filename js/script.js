9ac6c049ef0f46e80d8f472f61fa9a0b4ca14dd6
Mukhammadjon <muhammadjonabdurashidov2@gmail.com>
2022-09-06 16:39:21 +0500

    git add

---
+++ js/script.js
@@ -0,0 +1,12 @@
+alert ("“Welcome!”");
+let yourName = prompt ("ismingiz!");
+let lastName = prompt ("familiyangiz!");
+let yourAge = prompt ("yoshingiz!");
+let yourAddress = prompt ("Manzilingiz");
+let trueFalse = confirm ("saytga haqiqatdan ham kirasizmi!");
+alert ("rahmat");
+
+console.log(` ism: ${yourName}
+ familiya: ${lastName} 
+ yosh: ${yourAge} 
+ manzil: ${yourAddress}`);
